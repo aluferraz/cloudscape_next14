2b0827e04eafa7dd300df303d29470b51139950e
Lucas Mohallem Ferraz <ferraz.m.lucas@gmail.com>
2024-03-03 04:26:33 -0600

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,31 @@
+import { type AppType } from "next/app";
+import { Inter } from "next/font/google";
+
+import { api } from "~/utils/api";
+import '@cloudscape-design/global-styles'
+
+import "~/styles/globals.css";
+import React from 'react'
+if (typeof window === 'undefined'){ 
+  React.useLayoutEffect = () => {
+    console.log("Disable useLayoutEffect")
+  }
+  React.useEffect = () => {
+    console.log("Disable useLayoutEffect")
+  }
+}
+
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <main className={`font-sans ${inter.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
+};
+
+export default api.withTRPC(MyApp);
